857000049b10b2ed89a90bf7e285f8f24ff77cec
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-05-01 12:25:51 +0300

    Create dispatcher.py

---
+++ bot_service/app/bot/dispatcher.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from app.core.config import settings
+from app.bot.handlers import router
+
+bot = Bot(token=settings.TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher()
+dp.include_router(router)
