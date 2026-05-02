376316c06c4922e68169cd36d0c7c328078b9ffd
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-05-02 08:44:33 +0300

    Update dispatcher.py

--- bot_service/app/bot/dispatcher.py
+++ bot_service/app/bot/dispatcher.py
@@ -5,11 +5,12 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from aiohttp_socks import ProxyConnector
 from app.core.config import settings
 
-# Функция для создания бота с прокси
+
+#  логин и пароль 
+PROXY_URL = "http://nthtRt:KAyary@45.129.184.86:8000"
+
+# Функция для создания бота
 async def create_bot():
-    """Создает экземпляр бота с SOCKS5 прокси"""
-    PROXY_URL = "socks5://nthtRt:KAyary@45.129.184.86:8000"
-    
     connector = ProxyConnector.from_url(PROXY_URL)
     session = AiohttpSession(connector=connector)
     
@@ -18,15 +19,14 @@ async def create_bot():
         session=session,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
-#  логин и пароль 
-# PROXY_URL = "http://nthtRt:KAyary@45.129.184.86:8000"
 
-# Создаем бота позже, при старте
-bot = None
-dp = Dispatcher()
+# Ленивая инициализация
+_bot = None
 
 async def get_bot():
-    global bot
-    if bot is None:
-        bot = await create_bot()
-    return bot
+    global _bot
+    if _bot is None:
+        _bot = await create_bot()
+    return _bot
+
+dp = Dispatcher()
