df0c4c2d1bfea99c225842ef08ca8be36df3857f
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-05-02 08:11:04 +0300

    Update dispatcher.py

--- bot_service/app/bot/dispatcher.py
+++ bot_service/app/bot/dispatcher.py
@@ -2,12 +2,14 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
+from aiohttp_socks import ProxyConnector
 from app.core.config import settings
 
-# ЗАМЕНИТЕ логин и пароль на ваши
+#  логин и пароль 
 PROXY_URL = "http://nthtRt:KAyary@45.129.184.86:8000"
 
-session = AiohttpSession(proxy=PROXY_URL)
+connector = ProxyConnector.from_url(PROXY_URL)
+session = AiohttpSession(connector=connector)
 
 bot = Bot(
     token=settings.TELEGRAM_BOT_TOKEN,
