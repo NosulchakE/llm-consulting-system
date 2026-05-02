6cfc57c5414907326f18967738ec49580ddf4587
Elena Nosulchak <133764080+NosulchakE@users.noreply.github.com>
2026-05-02 09:06:14 +0300

    Update dispatcher.py

--- bot_service/app/bot/dispatcher.py
+++ bot_service/app/bot/dispatcher.py
@@ -2,31 +2,21 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
-from aiohttp_socks import ProxyConnector
 from app.core.config import settings
 
 
 #  логин и пароль 
 PROXY_URL = "http://nthtRt:KAyary@45.129.184.86:8000"
 
-# Функция для создания бота
-async def create_bot():
-    connector = ProxyConnector.from_url(PROXY_URL)
-    session = AiohttpSession(connector=connector)
-    
-    return Bot(
-        token=settings.TELEGRAM_BOT_TOKEN,
-        session=session,
-        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
-    )
+# Создаем сессию с прокси
+session = AiohttpSession(proxy=PROXY_URL)
 
-# Ленивая инициализация
-_bot = None
-
-async def get_bot():
-    global _bot
-    if _bot is None:
-        _bot = await create_bot()
-    return _bot
+bot = Bot(
+    token=settings.TELEGRAM_BOT_TOKEN,
+    session=session,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 
 dp = Dispatcher()
+
+
